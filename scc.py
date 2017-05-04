c9c8b768a03175822cfcfc0674db63fa06171b47
Abubakr <abubakr.a.hassan@semicolon-ict.com>
2017-05-04 11:44:45 +0300

    modified vizualizer

--- scc.py
+++ scc.py
@@ -1,3 +1,5 @@
+from ast import visualizer
+from ast.visualizer import Visualizer
 from parser_ import Parser
 from lexer import Lexer
 from compiler.compiler import Compiler
@@ -13,5 +15,7 @@ void main(){
 lex = Lexer(text=code)
 parser = Parser(lexer=lex)
 comp = Compiler(parser=parser)
+viz = Visualizer(parser=parser)
 
-print(comp.compile())
+
+viz.visualize()
