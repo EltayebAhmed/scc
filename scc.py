9adf30cc63ebde92ec555b5f687b77b58920c32d
Abdallah Mohammed <abdullahbashir077@gmail.com>
2017-05-05 10:21:20 +0300

    finish working on arithmetic expressions

--- scc.py
+++ scc.py
@@ -1,18 +1,18 @@
-#from ast import visualizer
-#from ast.visualizer import Visualizer
+from ast import visualizer
+from ast.visualizer import Visualizer
 from parser_ import Parser
 from lexer import Lexer
 from compiler.compiler import Compiler
 code = """
 void main(){
-    printf(3 + 12*5);
+    printf(3--3);
 }
 """
 
 lex = Lexer(text=code)
 parser = Parser(lexer=lex)
 comp = Compiler(parser=parser)
-#viz = Visualizer(parser=parser)
-
+viz = Visualizer(parser=parser)
+viz.visualize()
 
 print(comp.compile())
